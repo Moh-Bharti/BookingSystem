cfa2ae66faf5d3bc4046e94041db10c80b1d4d15
sanchit16264 <sanchit16264@iiitd.ac.in>
2017-10-23 23:39:55 +0530

    Main login page hai

---
+++ MainController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainController {
+	@FXML
+	private Button m ;
+	@FXML
+	private TextField em1 ;
+	@FXML
+	private PasswordField ps2 ;
+	@FXML
+	private Label Head ;
+}
---
+++ SignUpController.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+@FXML
+private Button m;
+
+@FXML
+private PasswordField m1;
+@FXML
+private PasswordField m2;
+@FXML
+private Label m3;
+}
