7392f83c3bf3606a88f4c5a7fc8968cb6f1a0f82
sanchit16264 <sanchit16264@iiitd.ac.in>
2017-10-24 09:46:21 +0530

    Add files via upload

---
+++ FacultyController.java
@@ -0,0 +1,28 @@
+package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+public class FacultyController {
+	@FXML
+	private Button b1 ;
+	@FXML
+	private Button b2 ;
+	@FXML
+	private Button b3 ;
+	@FXML
+	private Button b4 ;
+	@FXML
+	private Button b5 ;
+	@FXML
+	private Button b6 ;
+	@FXML
+	private Button b7 ;
+	@FXML
+	private TextField em1 ;
+	@FXML
+	private PasswordField ps2 ;
+	@FXML
+	private Label Head ;
+}
---
+++ SignUpController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+@FXML
+private Button m;
+@FXML
+private Button m6;
+@FXML
+private Button m7;
+
+@FXML
+private PasswordField m1;
+@FXML
+private PasswordField m2;
+@FXML
+private Label m3;
+@FXML
+private TextField m4;
+}
